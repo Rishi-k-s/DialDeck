6053a42abbd5c94824437deda8b2d01c86794fdc
Rishi-k-s <rishikrishna.sr@gmail.com>
2024-11-21 02:01:49 +0530

    building the User Interface

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
 from PyQt6.QtQuick import QQuickWindow
 
-QQuickWindow.setSceneGraphBackend('software')
-app = QGuiApplication(sys.argv)
+def windowInit():
+    QQuickWindow.setSceneGraphBackend('software')
+    app = QGuiApplication(sys.argv)
 
-engine = QQmlApplicationEngine()
-engine.quit.connect(app.quit)
-engine.load('./UI/main.qml')
+    engine = QQmlApplicationEngine()
+    engine.quit.connect(app.quit)
+    engine.load('./UI/main.qml')
 
-sys.exit(app.exec())
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    windowInit()
